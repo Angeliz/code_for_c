23f03924040ecd33d089952fc3e01d251a408703
Angeliz <471330418@qq.com>
2017-10-08 22:53:00 +0800

    还没弄好

---
+++ reciprocal.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// #include <ctype.h>
+// #include <math.h>
+// #include <stdlib.h>
+// int main(){
+// 	double x;
+// 	double s=0;
+// 	s=1.0/x;
+// 	scanf("%lf", &x);
+// 	printf("%lf", &s);
+// 	return 0;
+// }
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("hello world!\n");
+    return 0;
+}
